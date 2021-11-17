434d69a8a01e77087d8750e43788115892e9f780
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2021-11-17 20:50:09 +0100

    Update ucenjegit.sql

--- ucenjegit.sql
+++ ucenjegit.sql
@@ -1,3 +1,11 @@
 drop database if exists ucenjegit;
 create database ucenjegit;
 use ucenjegit;
+
+create table git(
+    sifra int;
+    naziv varchar(50);
+    tip int
+);
+
+insert into git(sifra,naziv) values (null,'jeeej');
