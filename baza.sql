f14a75af35a45b6e058ef6a881e0367997cdc619
Miroslav Ambroš <Miroslav.ambros@gmail.com>
2021-11-20 16:28:32 +0100

    Create baza.sql

---
+++ baza.sql
@@ -0,0 +1,26 @@
+drop database if exists baza;
+create database baza;
+use baza;
+create table zupanija (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    zupan int
+);
+create table opcina (
+    sifra int not null primary key auto_increment,
+    zupanija int not null,
+    naziv varchar(50)
+);
+create table mjesto(
+    sifra int not null primary key auto_increment,
+    opcina int not null,
+    naziv varchar(50)
+);
+create table zupan (
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50)
+);
+alter table opcina add foreign key (zupanija) references zupanija(sifra);
+alter table zupanija add foreign key (zupan) references zupan(sifra);
+alter table mjesto add foreign key (opcina) references opcina(sifra);
