40e386260375c1a947c6d241337e751fcdfe8c82
MiroslavAmbros <Miroslav.ambros@gmail.com>
2021-11-22 19:40:02 +0100

    update sa nastave

--- zavrsni rad/Autoservis.sql
+++ zavrsni rad/Autoservis.sql
@@ -2,7 +2,7 @@ drop database if exists autoservis;
 create database autoservis character set utf8;
 # c:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < C:\jp25\jp25sql\zavrsni_rad\autoservis.sql
 use autoservis;
-create table servispartner (
+create table servis (
     sifra int not null primary key auto_increment,
     naziv varchar(50) not null,
     adresa varchar(50) not null,
@@ -16,22 +16,32 @@ create table djelatnik(
     prezime varchar(50) not null,
     oib char(11),
     iban varchar(50),
-    servispartner int
+    servis int
 );
-create table servisninalog(
+create table radni_nalog(
     sifra int not null primary key auto_increment,
-    naziv varchar(50) not null,
-    brojsasije varchar(50),
+    rednibroj int auto_increment,
     opisservisa varchar(250),
     datum datetime,
     kilometraza varchar(50),
     djelatnik int
 );
+create table vozilo (
+    registracija varchar(50),
+    vlasnik int
+    brojsasije varchar(50)
+);
+create table vlasnik (
+    ime varchar(50),
+    prezime varchar(50),
+    oib char(11),
+    brojtelefona varchar(50)
+);
 
-alter table djelatnik add foreign key (servispartner) references servispartner(sifra);
+alter table djelatnik add foreign key (servis) references servis(sifra);
 alter table servisninalog add foreign key (djelatnik) references djelatnik(sifra);
 #servis parteri
-insert into servispartner(sifra,naziv,adresa)
+insert into servis(sifra,naziv,adresa)
 values
 (null,'AS Štimac','Gornjodravska 66, Osijek'),
 (null,'AUTOPOINT','Kišpatićeva 99, Osijek');
@@ -45,19 +55,19 @@ values
 (null,'Vlado','Kanteršić'),
 (null,'Aleksandar','Vukobratović');
 
-update servispartner 
+update servis 
 set oib='06860590644' 
 where sifra=1;
 
-update servispartner
+update servis
 set iban='HR4523400098274733217'
 where sifra=1;
 
-update servispartner 
+update servis 
 set oib='27808967531'
 where sifra=2;
 
-update servispartner
+update servis
 set iban='HR5823600005152285761'
 where sifra=2;
 
@@ -77,6 +87,6 @@ update djelatnik
 set oib='54092461342'
 where sifra=4;
 
-update servispartner 
+update servis 
 set licenciran=true
 where sifra=1;
